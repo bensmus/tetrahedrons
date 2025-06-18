03cedbc7362817b179cd5b10238ec356f7ebbd84
Ben Smus <bensmus@gmail.com>
2025-06-17 23:45:21 -0700

    Relaxing nicely spinning

--- main.js
+++ main.js
@@ -12,16 +12,15 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.TetrahedronGeometry();
+const geometry = new THREE.TetrahedronGeometry(2);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const tetra = new THREE.Mesh(geometry, material);
+scene.add(tetra);
 
 camera.position.z = 5;
 
 function animate() {
-  cube.rotation.x += 0.002;
-  cube.rotation.y += 0.002;
+  tetra.rotation.y += 0.0005;
   renderer.render(scene, camera);
 }
 
