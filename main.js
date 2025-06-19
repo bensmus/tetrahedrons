8111b4dfa70d0e1fe95bfda550ad3a9ba3f5556a
Ben Smus <bensmus@gmail.com>
2025-06-19 09:20:59 -0700

    Will try to index geometry

--- main.js
+++ main.js
@@ -13,8 +13,11 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.TetrahedronGeometry(2);
+console.log(geometry.attributes.position); // Why 12 points? Because geometry is non-indexed and has 3 points per triangle.
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 const tetra = new THREE.Mesh(geometry, material);
+const axis = new THREE.Vector3(1, 1, 1);
+tetra.translateOnAxis(axis, 1);
 scene.add(tetra);
 
 camera.position.z = 5;
