0d62db01a0df009ab65f9059d806cd8664bbc4ba
Ben Smus <bensmus@gmail.com>
2025-06-23 00:11:33 -0700

    Have idea

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// GOAL: make 4 tetrahedrons next to each other, making a larger single tetrahedron.
+
+// FIXME: Use inbuilt tetrahedron,
+// determine the correct 3 shift vectors.
+
+// Can use this for sierp(x, y, z, edgeLength),
+// only add the smallest tetrahedrons to the scene (no need for transparency).
+
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
@@ -12,81 +20,16 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// FIXME: For easier translation, a half size tetrahedron
-// could have common vertex.
-function getTetrahedronVectors(triSize) {
-  const v0 = new THREE.Vector3(1, 0, -Math.sqrt(2) / 2); // BOTTOM RIGHT
-  const v1 = new THREE.Vector3( // BOTTOM UP
-    Math.cos((2 * Math.PI) / 3),
-    Math.sin((2 * Math.PI) / 3),
-    -Math.sqrt(2) / 2
-  );
-  const v2 = new THREE.Vector3( // BOTTOM LEFT
-    Math.cos((4 * Math.PI) / 3),
-    Math.sin((4 * Math.PI) / 3),
-    -Math.sqrt(2) / 2
-  );
-  const v3 = new THREE.Vector3(0, 0, Math.sqrt(2) / 2); // TOP
-  const vectors = [v0, v1, v2, v3];
-  return vectors.map((v) => v.multiplyScalar(triSize));
-}
+const geometry = new THREE.TetrahedronGeometry();
+const material = new THREE.MeshNormalMaterial({ wireframe: true });
+const mesh = new THREE.Mesh(geometry, material);
 
-function getTetrahedronGeometry(tetrahedronVectors) {
-  // All coordinates i.e. point buffer:
-  const tetrahedronCoordinates = [
-    tetrahedronVectors[0].x,
-    tetrahedronVectors[0].y,
-    tetrahedronVectors[0].z,
-    tetrahedronVectors[1].x,
-    tetrahedronVectors[1].y,
-    tetrahedronVectors[1].z,
-    tetrahedronVectors[2].x,
-    tetrahedronVectors[2].y,
-    tetrahedronVectors[2].z,
-    tetrahedronVectors[3].x,
-    tetrahedronVectors[3].y,
-    tetrahedronVectors[3].z,
-  ];
-
-  // Counter-clockwise triangles:
-  const tetrahedronTris = [0, 1, 2, 0, 3, 2, 0, 3, 1, 2, 3, 1];
-
-  const tetrahedronGeometry = new THREE.PolyhedronGeometry(
-    tetrahedronCoordinates,
-    tetrahedronTris,
-    1,
-    0
-  );
-
-  return tetrahedronGeometry;
-}
-
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
-});
-
-const tetra1 = new THREE.Mesh(
-  getTetrahedronGeometry(getTetrahedronVectors(1)),
-  material
-);
-
-const tetra2 = new THREE.Mesh(
-  getTetrahedronGeometry(getTetrahedronVectors(4)),
-  material
-);
-
-// const axis = new THREE.Vector3(1, 1, 1);
-// tetra1.translateOnAxis(axis, 1);
-scene.add(tetra1);
-scene.add(tetra2);
-tetra2.position.x = 4;
+scene.add(mesh);
 
 camera.position.z = 5;
 
 function animate() {
-  tetra1.rotation.y += 0.0005;
-  tetra2.rotation.y += 0.0005;
+  mesh.rotation.y += 0.0005;
   renderer.render(scene, camera);
 }
 
